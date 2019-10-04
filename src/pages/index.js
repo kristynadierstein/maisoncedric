9b4072c9f367dbdbdbc0c232fcc1abc2f7461e76
Dev Maison Cedric <dev@maisoncedric.com>
2019-10-04 12:07:42 -0400

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,7 @@ import '../styles/main.scss';
 class Index extends React.Component {
   render(){
     return(
-      <h1> Rare tables de chevet laquées laiton de la Maison Jansen vers 1970.
-      <h2> Assises
-      <h3> Be the first to know
-      <h4> Assises
-      <h5> Maison Cédric
-      <p class="card title"> Etagère bibliothèque laquée laiton par kim moltzer 1970
-      <p class="card price"> 4.600 €
-      <p> Rares et exceptionnelles tables de chevet laquées et laiton de la Maison Jansen datant des années 70. Cette série laquée avec une finition bronze mordoré était produite à la commande uniquement… 
+      <p> Rares et exceptionnelles tables de chevet laquées et laiton de la Maison Jansen datant des années 70. Cette série laquée avec une finition bronze mordoré était produite à la commande uniquement…</p> 
       )
   }
 }
