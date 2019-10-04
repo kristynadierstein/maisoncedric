cbbf50c9f0bd2d5d24a2475616bf9a2c00e9e8fd
Dev Maison Cedric <dev@maisoncedric.com>
2019-10-04 12:12:25 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import '../styles/main.scss';
 class Index extends React.Component {
   render(){
     return(
-      <p> Rares et exceptionnelles tables de chevet laquées et laiton de la Maison Jansen datant des années 70. Cette série laquée avec une finition bronze mordoré était produite à la commande uniquement…</p> 
-      )
+      <p> Hello World! </p>
+    )
   }
 }
 
