e4180a9ecad49c566fd94242f635a1e2bb6d01a4
maisoncedric <55835127+maisoncedric@users.noreply.github.com>
2019-10-04 10:58:33 -0400

    Setup to view different typography variables
    As well as colors.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,14 @@ import '../styles/main.scss';
 class Index extends React.Component {
   render(){
     return(
-      <p> Hello World! </p>
+      <h1>Rare tables de chevet laquées laiton de la Maison Jansen vers 1970</h1>
+      <h2>Assises</h2>
+      <h3>Be the first to know</h3>
+      <h4>Assises</h4>
+      <h5>Maison Cédric</h5>
+      <p class="card title">Etagère bibliothèque laquée laiton par kim moltzer 1970</p>
+      <p class="card price">4.600 €</p>
+      <p>Rares et exceptionnelles tables de chevet laquées et laiton de la Maison Jansen datant des années 70. Cette série laquée avec une finition bronze mordoré était produite à la commande uniquement…</p>
       )
   }
 }
