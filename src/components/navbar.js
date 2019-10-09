8f20a2384c71ff76ee1a6ec26355684b3d4cc147
Alexandre Schmitz <amrschmitz@gmail.com>
2019-10-09 16:24:02 -0400

    merged master in current branch

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,10 @@
 import React from 'react';
-
-//import react-router-dom to be use Navlink and active tabs styling, once pages done, add switch to create routes
-import {
-    Route,
-    NavLink,
-    BrowserRouter as Router,
-    Switch,
-  } from 'react-router-dom';
+import { Link } from "gatsby";
 
 
 //import imagery
 import FlagEN from '../../static/images/ic--flag--en.svg';
-import FlagFR from '../../static/images/ic--flag--fr.svg';
+// import FlagFR from '../../static/images/ic--flag--fr.svg';
 
 class NavBar extends React.Component{
     constructor(props) {
@@ -29,25 +22,23 @@ class NavBar extends React.Component{
 
     render (){
         return(
-            <Router>
-                <nav id="navbar">
-                    <div id="navbar-left">
-                        <ul>
-                            <li><NavLink exact={true} activeClassName="active" to="#">Nouveautés</NavLink></li>
-                            <li><NavLink activeClassName="active" to="#">Assises</NavLink></li>
-                            <li><NavLink activeClassName="active" to="#">Mobilier</NavLink></li>
-                            <li><NavLink activeClassName="active" to="#">Luminaire</NavLink></li>
-                            <li><NavLink activeClassName="active" to="#">Décoration</NavLink></li>
-                        </ul>
-                    </div>
-                    <div id="navbar-right">
-                        <ul>
-                            <li><NavLink activeClassName="active" to="#">À propos</NavLink></li>
-                            <li><NavLink activeClassName="active" to="#">< FlagEN /></NavLink></li>
-                        </ul>
-                    </div>
-                </nav>
-            </Router>
+            <nav id="navbar">
+                <div id="navbar-left">
+                    <ul>
+                        <li><Link exact={true} activeClassName="active" to="">Nouveautés</Link></li>
+                        <li><Link activeClassName="active" to="/assises">Assises</Link></li>
+                        <li><Link activeClassName="active" to="/mobilier">Mobilier</Link></li>
+                        <li><Link activeClassName="active" to="/luminaires">Luminaires</Link></li>
+                        <li><Link activeClassName="active" to="/decoration">Décoration</Link></li>
+                    </ul>
+                </div>
+                <div id="navbar-right">
+                    <ul>
+                        <li><Link activeClassName="active" to="#">À propos</Link></li>
+                        <li><Link activeClassName="active" to="#">< FlagEN /></Link></li>
+                    </ul>
+                </div>
+            </nav>
         )
     }
 }
