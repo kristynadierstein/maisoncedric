1dfc70b29c0944f495cc04ac045c17ed03d19a71
Alexandre Schmitz <amrschmitz@gmail.com>
2019-10-09 01:26:12 -0400

    added category pages

--- src/pages/assises.js
+++ src/pages/assises.js
@@ -4,7 +4,99 @@ import React from "react";
 import '../styles/main.scss';
 
 export default () => (
-  <div className="container">
-    <h1>Assises</h1>
+  <div className="container category">
+    <div className="sidebar">
+      <h1>Assises</h1>
+      <hr/>
+      <ul>
+        <li>Toutes les assises</li>
+        <li>Fauteuils</li>
+        <li>Chaises</li>
+        <li>Canapés</li>
+        <li>Tabourets</li>
+        <li>Tables</li>
+        <li>Autres</li>
+      </ul>
+    </div>
+    <div className="row-3">
+      <div className="card">
+        <div className="image">
+          <div className="vendu">
+            <label htmlFor="">Vendu</label>
+          </div>
+        </div>
+        <div className="desc">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur facere corporis atque consequuntur.</p>
+          <label>
+            <strong>3.000 €</strong>
+          </label>
+        </div>
+      </div>
+      <div className="card">
+        <div className="image">
+          <div className="vendu">
+            <label htmlFor="">Vendu</label>
+          </div>
+        </div>
+        <div className="desc">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur facere corporis atque consequuntur.</p>
+          <label>
+            <strong>3.000 €</strong>
+          </label>
+        </div>
+      </div>
+      <div className="card">
+        <div className="image">
+          <div className="vendu">
+            <label htmlFor="">Vendu</label>
+          </div>
+        </div>
+        <div className="desc">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur facere corporis atque consequuntur.</p>
+          <label>
+            <strong>3.000 €</strong>
+          </label>
+        </div>
+      </div>
+      <div className="card">
+        <div className="image">
+          <div className="vendu">
+            <label htmlFor="">Vendu</label>
+          </div>
+        </div>
+        <div className="desc">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur facere corporis atque consequuntur.</p>
+          <label>
+            <strong>3.000 €</strong>
+          </label>
+        </div>
+      </div>
+      <div className="card">
+        <div className="image">
+          <div className="vendu">
+            <label htmlFor="">Vendu</label>
+          </div>
+        </div>
+        <div className="desc">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur facere corporis atque consequuntur.</p>
+          <label>
+            <strong>3.000 €</strong>
+          </label>
+        </div>
+      </div>
+      <div className="card">
+        <div className="image">
+          <div className="vendu">
+            <label htmlFor="">Vendu</label>
+          </div>
+        </div>
+        <div className="desc">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur facere corporis atque consequuntur.</p>
+          <label>
+            <strong>3.000 €</strong>
+          </label>
+        </div>
+      </div>
+    </div>
   </div>
 )
--- src/pages/decoration.js
+++ src/pages/decoration.js
@@ -4,7 +4,60 @@ import React from "react";
 import '../styles/main.scss';
 
 export default () => (
-  <div className="container">
-    <h1>Décoration</h1>
+  <div className="container category">
+    <div className="sidebar">
+      <h1>Décoration</h1>
+      <hr/>
+      <ul>
+        <li>Décoration type 1</li>
+        <li>Décoration type 2</li>
+        <li>Décoration type 3</li>
+        <li>Décoration type 4</li>
+        <li>Décoration type 5</li>
+        <li>Décoration type 6</li>
+        <li>Décoration type 7</li>
+      </ul>
+    </div>
+    <div className="row-3">
+      <div className="card">
+        <div className="image">
+          <div className="vendu">
+            <label htmlFor="">Vendu</label>
+          </div>
+        </div>
+        <div className="desc">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur facere corporis atque consequuntur.</p>
+          <label>
+            <strong>3.000 €</strong>
+          </label>
+        </div>
+      </div>
+      <div className="card">
+        <div className="image">
+          <div className="vendu">
+            <label htmlFor="">Vendu</label>
+          </div>
+        </div>
+        <div className="desc">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur facere corporis atque consequuntur.</p>
+          <label>
+            <strong>3.000 €</strong>
+          </label>
+        </div>
+      </div>
+      <div className="card">
+        <div className="image">
+          <div className="vendu">
+            <label htmlFor="">Vendu</label>
+          </div>
+        </div>
+        <div className="desc">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur facere corporis atque consequuntur.</p>
+          <label>
+            <strong>3.000 €</strong>
+          </label>
+        </div>
+      </div>
+    </div>
   </div>
 )
