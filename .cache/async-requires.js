9351e44eca247a1dff15a15fe85eb70819c0b660
Alexandre Schmitz <amrschmitz@gmail.com>
2019-10-09 03:22:10 -0400

    made the a propos page

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -3,6 +3,7 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---cache-dev-404-page-js": () => import("/Users/amrschmitz/code/maisoncedric/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-a-propos-js": () => import("/Users/amrschmitz/code/maisoncedric/src/pages/a-propos.js" /* webpackChunkName: "component---src-pages-a-propos-js" */),
   "component---src-pages-assises-js": () => import("/Users/amrschmitz/code/maisoncedric/src/pages/assises.js" /* webpackChunkName: "component---src-pages-assises-js" */),
   "component---src-pages-decoration-js": () => import("/Users/amrschmitz/code/maisoncedric/src/pages/decoration.js" /* webpackChunkName: "component---src-pages-decoration-js" */),
   "component---src-pages-index-js": () => import("/Users/amrschmitz/code/maisoncedric/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
