9ee650d8d12306fdd78b8c35afc4d518ff0a4730
Kristyna Dierstein <kristynahorankova@gmail.com>
2019-10-10 14:10:29 -0400

    inner sidebar styled, working

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,11 +2,10 @@ import React from 'react';
 import { Link } from "gatsby";
 import Sidebar from "react-sidebar";
 
-
-
 //import imagery
 import FlagEN from '../../static/images/ic--flag--en.svg';
 // import FlagFR from '../../static/images/ic--flag--fr.svg';
+import Esc from '../../static/images/ic--esc.svg';
 
 class NavBar extends React.Component{
     constructor(props) {
@@ -20,8 +19,13 @@ class NavBar extends React.Component{
     onSetSidebarOpen(open) {
         this.setState({ sidebarOpen: open });
       }
-    
 
+    handleClick = (e) => {
+        this.setState({
+            sidebarOpen: false
+        })
+    }
+    
     render (){
         return(
             <React.Fragment>
@@ -44,32 +48,35 @@ class NavBar extends React.Component{
                 </nav>
 
                 <nav id="mobile-navbar">
-                <Sidebar
-                    sidebar={
-                        <ul>
-                            <li><Link to="">Nouveautés</Link></li>
-                            <li><Link to="/assises">Assises</Link></li>
-                            <li><Link to="/mobilier">Mobilier</Link></li>
-                            <li><Link to="/luminaires">Luminaires</Link></li>
-                            <li><Link to="/decoration">Décoration</Link></li>
-                        </ul>
-                    }
-                    open={this.state.sidebarOpen}
-                    onSetOpen={this.onSetSidebarOpen}
-                    styles={{ sidebar: { background: "white" } }}
-                >
-                     <Link onClick={() => this.onSetSidebarOpen(true)} className="mb-navbar-btn-categories">
-                        Categories
-                    </Link>
-                </Sidebar>
-
+                    <Sidebar
+                        sidebar={
+                            <div>
+                                <div className="mb-sidebar-header">
+                                    <h3>Categories</h3>
+                                    < Esc onClick={this.handleClick}/>
+                                </div>
+                                <ul className="mb-sidebar-list">
+                                    <li><Link to="">Nouveautés</Link></li>
+                                    <li><Link to="/assises">Assises</Link></li>
+                                    <li><Link to="/mobilier">Mobilier</Link></li>
+                                    <li><Link to="/luminaires">Luminaires</Link></li>
+                                    <li><Link to="/decoration">Décoration</Link></li>
+                                </ul>
+                                <Link to="#" className="mb-sidebar-btn-contact">Contactez-nous </Link>
+                            </div>
+                        }
+                        open={this.state.sidebarOpen}
+                        onSetOpen={this.onSetSidebarOpen}
+                        sidebarClassName = "mobile-sidebar"
+                    >
+                        <Link onClick={() => this.onSetSidebarOpen(true)} className="mb-navbar-btn-categories">
+                            Categories
+                        </Link>
+                    </Sidebar>
                     <div className="mb-navbar-filtres">
                         <Link to="a-propos">À propos</Link>
                     </div> 
                 </nav>
-
-
-
             </React.Fragment>
         )
     }
