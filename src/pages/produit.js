108bc83f9e751eb9b0b6c1bd426f35b8067a43e6
Kristyna Dierstein <kristynahorankova@gmail.com>
2019-11-05 11:09:06 -0500

    mobile-links styled and working

--- src/pages/produit.js
+++ src/pages/produit.js
@@ -1,4 +1,7 @@
+//import libraries
 import React from "react";
+import MediaQuery from 'react-responsive';
+import Modal from 'react-responsive-modal';
 
 // import styles
 import '../styles/main.scss';
@@ -8,8 +11,9 @@ import Header from '../components/header.js';
 import Navbar from '../components/navbar.js';
 import Footer from '../components/footer.js';
 import NewsletterAd from '../components/newsletterAd.js';
-import Carousel from '../components/carousel';
-import Modal from 'react-responsive-modal';
+import Carousel from '../components/carousel.js';
+import MobileLinks from '../components/mobile-links.js';
+
 
 //import imagery
 import Env from '../../static/images/ic-mail.svg';
@@ -42,6 +46,12 @@ class Produit extends React.Component {
             </div>
             <div className="content">
               <h1>Rare table de chevet laquée laiton de la Maison Jansen (1970)</h1>
+              <MediaQuery maxDeviceWidth={1199}>   
+              <div className="fixed-mobile-links">  
+                  <MobileLinks />
+              </div>
+              </MediaQuery>
+              <MediaQuery minDeviceWidth={1199}>
               <hr/>
               <div className="call-to-action">
                 <label htmlFor="">Vendu</label>
@@ -64,6 +74,7 @@ class Produit extends React.Component {
                 </Modal>
               </div>
               <hr/>
+              </MediaQuery>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo quasi iusto ab placeat non sed recusandae dignissimos quod. Sit adipisci asperiores veritatis nisi nihil. Nesciunt omnis excepturi totam porro hic.
               </p>
