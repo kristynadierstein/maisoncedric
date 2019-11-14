5efdac09d733fee1441040c05a8dbf3f044ab164
Alexandre Schmitz <amrschmitz@gmail.com>
2019-11-14 16:07:35 -0500

    added the translation on mobile

--- src/components/mobile-navbar.js
+++ src/components/mobile-navbar.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Link } from "gatsby";
 import Sidebar from "react-sidebar";
 import Modal from 'react-responsive-modal';
+import { injectIntl, FormattedMessage } from "gatsby-plugin-intl";
 
 //import imagery
 import Esc from '../../static/images/ic--esc.svg';
@@ -17,76 +18,74 @@ class MobileNavbar extends React.Component{
       sidebarOpen: false,
       open: false,
     };
-
     this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
     this.handleClick = this.handleClick.bind(this);
+  }
 
-  };
-  
   onSetSidebarOpen = (open) => {
     this.setState({ sidebarOpen: open });
   }
-  
+
   handleClick = (e) => {
     this.setState({ sidebarOpen: false });
   }
-  
+
   onOpenModal = () => {
     this.setState({ open: true });
-  };
-  
+  }
+
   onCloseModal = () => {
     this.setState({ open: false });
-  };
-  
+  }
+
   render (){
     const activeStyle = {
       color: '#17181A',
       fontWeight: 600,
     }
-    
+
     const { open } = this.state;
 
     return(
-      <React.Fragment>     
-      <nav id="mobile-navbar">
-        <Sidebar
-          sidebar={
-          <div>
-            <div className="mb-sidebar-header">
-              <h3>Categories</h3>
-              < Esc onClick={this.handleClick}/>
-            </div>
-            <ul className="mb-sidebar-list">
-              <li><Link to="/" activeStyle={activeStyle} >Nouveautés</Link></li>
-              <li><Link to="/assises" activeStyle={activeStyle} >Assises</Link></li>
-              <li><Link to="/mobilier" activeStyle={activeStyle} >Mobilier</Link></li>
-              <li><Link to="/luminaires" activeStyle={activeStyle} >Luminaires</Link></li>
-              <li><Link to="/decoration" activeStyle={activeStyle} >Décoration</Link></li>
-            </ul>
-          <div onClick={this.onOpenModal} className="mb-sidebar-btn-contact">Contactez-nous </div>
-          <Modal open={open} onClose={this.onCloseModal} closeIconSize={20} center closeIcon="closeIcon">
-            <ModalContent />
-          </Modal>
-          </div>
-          }
-          open={this.state.sidebarOpen}
-          onSetOpen={this.onSetSidebarOpen}
-          sidebarClassName = "mobile-sidebar"
+      <React.Fragment>
+        <nav id="mobile-navbar">
+          <Sidebar
+            sidebar={
+              <div>
+                <div className="mb-sidebar-header">
+                  <h3><FormattedMessage id="navbar.tabs_title" /></h3>
+                  <Esc onClick={this.handleClick}/>
+                </div>
+                <ul className="mb-sidebar-list">
+                  <li><Link to="/" activeStyle={activeStyle} ><FormattedMessage id="navbar.tab1" /></Link></li>
+                  <li><Link to="/assises" activeStyle={activeStyle} ><FormattedMessage id="navbar.tab2" /></Link></li>
+                  <li><Link to="/mobilier" activeStyle={activeStyle} ><FormattedMessage id="navbar.tab3" /></Link></li>
+                  <li><Link to="/luminaires" activeStyle={activeStyle} ><FormattedMessage id="navbar.tab4" /></Link></li>
+                  <li><Link to="/decoration" activeStyle={activeStyle} ><FormattedMessage id="navbar.tab5" /></Link></li>
+                </ul>
+              <div onClick={this.onOpenModal} className="mb-sidebar-btn-contact">Contactez-nous </div>
+              <Modal open={open} onClose={this.onCloseModal} closeIconSize={20} center closeIcon="closeIcon">
+                <ModalContent />
+              </Modal>
+              </div>
+            }
+            open={this.state.sidebarOpen}
+            onSetOpen={this.onSetSidebarOpen}
+            sidebarClassName = "mobile-sidebar"
           >
           <div onClick={() => this.onSetSidebarOpen(true)} className="mb-navbar-btn-categories" >
           <div className="mb-navbar-empty-category">
-            Categories
+            <FormattedMessage id="navbar.tabs_title" />
           </div>
         </div>
         </Sidebar>
         <div className="mb-navbar-apropos">
-          <Link to="/a-propos" activeStyle={activeStyle} >À propos</Link>
+          <Link to="/a-propos" activeStyle={activeStyle} ><FormattedMessage id="navbar.tab6" /></Link>
         </div>
       </nav>
-      </React.Fragment>
-      )
-    }
-  } 
+    </React.Fragment>
+    )
+  }
+}
 
-  export default MobileNavbar
+export default injectIntl(MobileNavbar)
