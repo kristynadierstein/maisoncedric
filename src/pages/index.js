7bac93d0b9543620cf3ee3e852cf256c62c69b3f
Alexandre Schmitz <amrschmitz@gmail.com>
2019-11-18 22:14:21 -0500

    made only the new items appear on the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,27 @@
 // external libraries
-import React from 'react';
-import {graphql} from 'gatsby';
-import MediaQuery from 'react-responsive';
-import { injectIntl, FormattedMessage } from "gatsby-plugin-intl";
+import React from 'react'
+import {graphql} from 'gatsby'
+import MediaQuery from 'react-responsive'
+import { injectIntl, FormattedMessage } from "gatsby-plugin-intl"
 
 // import styles
-import '../styles/main.scss';
+import '../styles/main.scss'
 
 //import components
-import Card from '../components/card.js';
-import Footer from '../components/footer.js';
-import Header from '../components/header.js';
-import MobileNavbar from '../components/mobile-navbar.js';
-import Navbar from '../components/navbar.js';
-import NewsletterAd from '../components/newsletterAd.js';
+import Card from '../components/card.js'
+import Footer from '../components/footer.js'
+import Header from '../components/header.js'
+import MobileNavbar from '../components/mobile-navbar.js'
+import Navbar from '../components/navbar.js'
+import NewsletterAd from '../components/newsletterAd.js'
 
 class Index extends React.Component {
   render(){
     // data from Airtable
-    const nodes = this.props.data.allAirtable.nodes;
+    const nodes = this.props.data.allAirtable.nodes
 
     // current language
-    const locale = this.props.intl.locale;
+    const locale = this.props.intl.locale
 
     // gives product title according to current language
     const getLocalizedProductTitle = (locale, data) => {
@@ -31,6 +31,8 @@ class Index extends React.Component {
         return data.Titre_de_l_annonce__FR_
       }
     }
+
+    console.log(nodes)
 
     return (
       <React.Fragment>
@@ -46,58 +48,79 @@ class Index extends React.Component {
           <h1><FormattedMessage id="index.titre" /></h1>
 
           <div className="row-3">
-            <Card
-              title={getLocalizedProductTitle(locale, nodes[4].data)}
-              price={nodes[4].data.Prix_de_vente}
-              status={nodes[4].data.Statut}
-              image={nodes[4].data.Images[0].url}
-              id={nodes[4].data.ID}>
-            </Card>
-            <Card
-              title={getLocalizedProductTitle(locale, nodes[3].data)}
-              price={nodes[3].data.Prix_de_vente}
-              status={nodes[3].data.Statut}
-              image={nodes[3].data.Images[0].url}
-              id={nodes[3].data.ID}>
-            </Card>
-            <Card
-              title={getLocalizedProductTitle(locale, nodes[2].data)}
-              price={nodes[2].data.Prix_de_vente}
-              status={nodes[2].data.Statut}
-              image={nodes[2].data.Images[0].url}
-              id={nodes[2].data.ID}>
-            </Card>
+            {(0 < nodes.length - 1) &&
+            // Makes the card appear only if the item exists
+              <Card
+                title={getLocalizedProductTitle(locale, nodes[0].data)}
+                price={nodes[0].data.Prix_de_vente}
+                status={nodes[0].data.Statut}
+                image={nodes[0].data.Images[0].url}
+                id={nodes[0].data.ID}>
+              </Card>
+            }
+            {(1 < nodes.length - 1) &&
+            // Makes the card appear only if the item exists
+              <Card
+                title={getLocalizedProductTitle(locale, nodes[1].data)}
+                price={nodes[1].data.Prix_de_vente}
+                status={nodes[1].data.Statut}
+                image={nodes[1].data.Images[0].url}
+                id={nodes[1].data.ID}>
+              </Card>
+            }
+            {(2 < nodes.length - 1) &&
+            // Makes the card appear only if the item exists
+              <Card
+                title={getLocalizedProductTitle(locale, nodes[2].data)}
+                price={nodes[2].data.Prix_de_vente}
+                status={nodes[2].data.Statut}
+                image={nodes[2].data.Images[0].url}
+                id={nodes[2].data.ID}>
+              </Card>
+            }
           </div>
 
           <div className="row-4">
-            <Card
-              title={getLocalizedProductTitle(locale, nodes[0].data)}
-              price={nodes[0].data.Prix_de_vente}
-              status={nodes[0].data.Statut}
-              image={nodes[0].data.Images[0].url}
-              id={nodes[0].data.ID}>
-            </Card>
-            <Card
-              title={getLocalizedProductTitle(locale, nodes[1].data)}
-              price={nodes[1].data.Prix_de_vente}
-              status={nodes[1].data.Statut}
-              image={nodes[1].data.Images[0].url}
-              id={nodes[1].data.ID}>
-            </Card>
-            <Card
-              title={getLocalizedProductTitle(locale, nodes[2].data)}
-              price={nodes[2].data.Prix_de_vente}
-              status={nodes[2].data.Statut}
-              image={nodes[2].data.Images[0].url}
-              id={nodes[2].data.ID}>
-            </Card>
-            <Card
-              title={getLocalizedProductTitle(locale, nodes[3].data)}
-              price={nodes[3].data.Prix_de_vente}
-              status={nodes[3].data.Statut}
-              image={nodes[3].data.Images[0].url}
-              id={nodes[3].data.ID}>
-            </Card>
+            {(3 < nodes.length - 1) &&
+            // Makes the card appear only if the item exists
+              <Card
+                title={getLocalizedProductTitle(locale, nodes[3].data)}
+                price={nodes[3].data.Prix_de_vente}
+                status={nodes[3].data.Statut}
+                image={nodes[3].data.Images[0].url}
+                id={nodes[3].data.ID}>
+              </Card>
+            }
+            {(4 < nodes.length - 1) &&
+            // Makes the card appear only if the item exists
+              <Card
+                title={getLocalizedProductTitle(locale, nodes[4].data)}
+                price={nodes[4].data.Prix_de_vente}
+                status={nodes[4].data.Statut}
+                image={nodes[4].data.Images[0].url}
+                id={nodes[4].data.ID}>
+              </Card>
+            }
+            {(5 < nodes.length - 1) &&
+            // Makes the card appear only if the item exists
+              <Card
+                title={getLocalizedProductTitle(locale, nodes[5].data)}
+                price={nodes[5].data.Prix_de_vente}
+                status={nodes[5].data.Statut}
+                image={nodes[5].data.Images[0].url}
+                id={nodes[5].data.ID}>
+              </Card>
+            }
+            {(6 < nodes.length - 1) &&
+            // Makes the card appear only if the item exists
+              <Card
+                title={getLocalizedProductTitle(locale, nodes[6].data)}
+                price={nodes[6].data.Prix_de_vente}
+                status={nodes[6].data.Statut}
+                image={nodes[6].data.Images[0].url}
+                id={nodes[6].data.ID}>
+              </Card>
+            }
           </div>
         </div>
         <NewsletterAd />
@@ -107,12 +130,12 @@ class Index extends React.Component {
   }
 }
 
-export default injectIntl(Index);
+export default injectIntl(Index)
 
 // Airtable query
 export const query = graphql`
   query MyIndexQuery {
-    allAirtable(sort: {fields: data___Created_Time, order: DESC}, filter: {data: {Images: {elemMatch: {size: {gt: 1}}}}}) {
+    allAirtable(sort: {fields: data___Created_Time, order: DESC}, filter: {data: {Images: {elemMatch: {size: {gt: 1}}}, Nouveaut__New: {eq: true}}}) {
       nodes {
         data {
           ID
@@ -129,4 +152,4 @@ export const query = graphql`
       }
     }
   }
-`;
+`
